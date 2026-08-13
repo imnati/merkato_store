8ccf987df5ebe74c95ec1bc9c925ebd934436710
NS <Nigus.Solomon.Dev@outlook.com>
2026-08-13 23:47:07 +0300

    chore(config): migrate next.config to ESM with image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "merkatostore.com",
+      },
+    ],
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
